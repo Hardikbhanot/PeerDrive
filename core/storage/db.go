701841240e6514f86502014221e6f34d18cc55fa
Hardik Bhanot <hardikbhanot123@gmail.com>
2026-09-03 00:06:47 +0530

    Fix: change sqlite driver name to sqlite for modernc

--- core/storage/db.go
+++ core/storage/db.go
@@ -24,7 +24,7 @@ type Share struct {
 }
 
 func InitDB(dbPath string) (*DB, error) {
-	conn, err := sql.Open("sqlite3", dbPath)
+	conn, err := sql.Open("sqlite", dbPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %w", err)
 	}
